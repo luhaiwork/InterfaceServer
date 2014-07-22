850c77c6ccc9e1bb26d03eea20b329934fc25a8b
luhai <luhaiwork@gmail.com>
2014-07-22 14:50:52 +0800

    firt commit . Add jersey,spring.

---
+++ InterfaceServer/src/main/java/com/template/transaction/TransactionBo.java
@@ -0,0 +1,5 @@
+package com.template.transaction;
+
+public interface TransactionBo {
+	String save();
+}
---
+++ InterfaceServer/src/main/java/com/template/transaction/impl/TransactionBoImpl.java
@@ -0,0 +1,11 @@
+package com.template.transaction.impl;
+
+import com.template.transaction.TransactionBo;
+
+public class TransactionBoImpl implements TransactionBo{
+	public String save() {
+		 
+		return "Jersey + Spring example";
+ 
+	}
+}
